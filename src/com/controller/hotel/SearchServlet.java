d58c7ede6e60eb0c374adffe899d97bdee5ecd33
woodbox0124 <32popo@naver.com>
2021-05-02 13:55:31 +0900

    호텔 리스트 지역 검색 수정
    지역 검색 시 페이지 안 보였던거 수정

--- src/com/controller/hotel/SearchServlet.java
+++ src/com/controller/hotel/SearchServlet.java
@@ -12,25 +12,46 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.dto.HotelDTO;
+import com.dto.PageDTO;
 import com.service.HotelService;
 
 @WebServlet("/SearchServlet")
 public class SearchServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		//페이지 리스트 화
+				String curPage = request.getParameter("curPage");//현재 페이지 얻기
+				if(curPage==null)curPage = "1";
+				
 		String location = request.getParameter("location");
-		
 		HotelService service = new HotelService();
+		PageDTO pDTO = null;
+		System.out.println(location);
+		System.out.println(pDTO);
 	    List<HotelDTO> list =null;
 	    
-		if(location!="") {
-			list = service.searchKey(location);
-		}else {
-			list = service.hotelList();
-		}
+	    String checkin = request.getParameter("date1"); 
+		  String checkout =request.getParameter("date2"); 
+		  String guest = request.getParameter("guest");
+
+		  if(location!="") { 
+		  pDTO = service.hotelList1(Integer.parseInt(curPage), location); 
+		  }else {
+		  pDTO = service.hotelList1(Integer.parseInt(curPage));
+		  } 
+		  request.setAttribute("pDTO", pDTO);
+		  request.setAttribute("location", location);
+		  System.out.println("pDTO"+pDTO);
+		  System.out.println("location"+location);
+		  
+		  HttpSession session = request.getSession(); 
+		  session.setAttribute("checkin",checkin); 
+		  session.setAttribute("checkout", checkout);
+		  session.setAttribute("location", location); 
+		  session.setAttribute("guest",guest);
         
 		
 		
