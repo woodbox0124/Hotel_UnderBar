ab7334537a21a202ac28c6cefc8bc150d336537e
HyunTori <so699@naver.com>
2021-05-10 17:27:18 +0900

    Board-Service,DAO,Mapper 주석 추가

--- src/com/dao/BoardDAO.java
+++ src/com/dao/BoardDAO.java
@@ -12,94 +12,108 @@ import com.dto.BoardPageDTO;
 import com.dto.HotelDTO;
 import com.dto.PageDTO;
 
-public class BoardDAO {	
+public class BoardDAO {
 	/*
 	 * public List<BoardDTO> listAll(SqlSession session, HashMap<String, String>
 	 * map) { List<BoardDTO> list =
 	 * session.selectList("com.board.BoardMapper.listAll", map); return list; }
 	 */
 
+	// 게시물 목록
+	public BoardPageDTO listAll(SqlSession session, HashMap<String, String> map, int curPage) {
+		BoardPageDTO pDTO = new BoardPageDTO();
+		int perPage = pDTO.getPerPage();
+		int offset = (curPage - 1) * perPage;
+		List<BoardDTO> list = session.selectList("com.board.BoardMapper.listAll", map, new RowBounds(offset, perPage));
+		pDTO.setCurPage(curPage);// 현재 페이지
+		pDTO.setList(list);// 리스트 저장
+
+		pDTO.setTotalCount(totalCount(session, map));// 검색어도 넘겨서 검색에 관련 전체 레코드 개수 구하기
+
+		return pDTO;
+	}
+	// 게시글 목록
+	
+	// 게시글 추가
 	public void write(SqlSession session, BoardDTO dto) {
 		System.out.println(dto);
-		int num= session.insert("com.board.BoardMapper.write", dto);
-		System.out.println("추가된 글 갯수 ======="+ num);		
+		int num = session.insert("com.board.BoardMapper.write", dto);
+		System.out.println("추가된 글 갯수 =======" + num);
+	}
+	// 게시글 추가
+
+	// 게시글 수정
+	public void updateByNum(SqlSession session, BoardDTO dto) {
+		int result = session.update("com.board.BoardMapper.updateByNum", dto);
+		System.out.println("업데이트 개수 =====" + result);
+	}
+	// 게시글 수정
+
+	// 게시글 삭제
+	public void deleteNum(SqlSession session, String num) {
+		int result = session.delete("com.board.BoardMapper.deleteByNum", Integer.parseInt(num));
+		System.out.println("삭제된 개수 =====" + result);
 	}
 
+	// 게시글 삭제
+	
+	// 글 조회수 증가
 	public BoardDTO selectByNum(SqlSession session, String num) {
-		// 글 조회수 증가
+
 		readCnt(session, num);
-		BoardDTO dto= session.selectOne("com.board.BoardMapper.selectByNum", Integer.parseInt(num));
+		BoardDTO dto = session.selectOne("com.board.BoardMapper.selectByNum", Integer.parseInt(num));
 		return dto;
 	}
 
 	private void readCnt(SqlSession session, String num) {
-		int result= session.update("com.board.BoardMapper.readCnt", Integer.parseInt(num));
-		System.out.println("result==="+ result);
-		
-	}
+		int result = session.update("com.board.BoardMapper.readCnt", Integer.parseInt(num));
+		System.out.println("result===" + result);
 
-	public void deleteNum(SqlSession session, String num) {
-		int result = session.delete("com.board.BoardMapper.deleteByNum", Integer.parseInt(num));
-		System.out.println("삭제된 개수 ====="+ result);
 	}
+	// 글 조회수 증가
 
-	public void updateByNum(SqlSession session, BoardDTO dto) {
-		int result= session.update("com.board.BoardMapper.updateByNum", dto);
-		System.out.println("업데이트 개수 ====="+ result);
-	}
+	// 댓글
+	public void answerinsert(SqlSession session, BoardDTO dto) {
 
-public void answerinsert(SqlSession session, BoardDTO dto) {
-		
 		int num = dto.getNum();
 		int origin = dto.getOrigin();
 		int groupnum = dto.getGroupnum();
 		int grouplayer = dto.getGrouplayer();
-		
-		if(num != 0)
-		{
-			layerupdate(session, origin, groupnum); //답글의 답글을 쓸때 그룹넘버를 +1 시켜주는 코드
+
+		if (num != 0) {
+			layerupdate(session, origin, groupnum); // 답글의 답글을 쓸때 그룹넘버를 +1 시켜주는 코드
 			groupnum++;
 			grouplayer++;
-		
+
 		}
-		
-		
+
 		dto.setOrigin(origin);
 		dto.setGroupnum(groupnum);
 		dto.setGrouplayer(grouplayer);
-		
-		int result= session.insert("com.board.BoardMapper.answerinsert", dto);
-		System.out.println("insert 개수 ====="+ result);
-				
-	}
-    public void layerupdate(SqlSession session, int origin, int groupnum){
-	 Map<String, Integer> map = new HashMap<String, Integer>();
-	 map.put("origin", origin);
-	 map.put("groupnum", groupnum);
-	 session.update("com.board.BoardMapper.updatelayer", map);	
-    }
 
-    public int totalCount(SqlSession session,HashMap<String, String> map) {	
-		int n=session.selectOne("com.board.BoardMapper.totalCount",map);
-		return n;					
-	}	
-    
-	public BoardPageDTO listAll(SqlSession session, HashMap<String, String> map, int curPage) {
-		BoardPageDTO pDTO = new BoardPageDTO();
-		int perPage = pDTO.getPerPage();
-		int offset = (curPage-1)*perPage;
-		List<BoardDTO> list = session.selectList("com.board.BoardMapper.listAll", map, new RowBounds(offset,perPage));
-		pDTO.setCurPage(curPage);//현재 페이지
-		pDTO.setList(list);//리스트 저장
-		
-		pDTO.setTotalCount(totalCount(session,map));//검색어도 넘겨서 검색에 관련 전체 레코드 개수 구하기
+		int result = session.insert("com.board.BoardMapper.answerinsert", dto);
+		System.out.println("insert 개수 =====" + result);
+
+	}
+	//댓글 
 	
-		return pDTO;
-	}	
-	public int getTotalCount(SqlSession session)
-	{
-	  int n=session.selectOne("com.board.BoardMapper.boardTotalCount");
-	  return n;
-	}	
+	// 대댓글
+	public void layerupdate(SqlSession session, int origin, int groupnum) {
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		map.put("origin", origin);
+		map.put("groupnum", groupnum);
+		session.update("com.board.BoardMapper.updatelayer", map);
+	}
+	// 대댓글
 	
+	public int totalCount(SqlSession session, HashMap<String, String> map) {
+		int n = session.selectOne("com.board.BoardMapper.totalCount", map);
+		return n;
+	}
+
+	public int getTotalCount(SqlSession session) {
+		int n = session.selectOne("com.board.BoardMapper.boardTotalCount");
+		return n;
+	}
+
 }
--- src/com/service/BoardService.java
+++ src/com/service/BoardService.java
@@ -13,22 +13,92 @@ import com.dto.BoardPageDTO;
 import com.dto.PageDTO;
 
 public class BoardService {
-  BoardDAO dao;
-  public BoardService() {
-	  dao= new BoardDAO();
-  }
-  public BoardDTO selectByNum(String num) {
-		SqlSession session= MySqlSessionFactory.getSession();
-		BoardDTO dto= null;
+	BoardDAO dao;
+
+	public BoardService() {
+		dao = new BoardDAO();
+	}
+
+	//게시글 목록
+	public BoardPageDTO listAll(int curPage, HashMap<String, String> map) {
+		BoardDAO dao = new BoardDAO();
+
+		SqlSession session = MySqlSessionFactory.getSession();
+		BoardPageDTO pDTO = null;
 		try {
-			dto= dao.selectByNum(session, num);
-			session.commit();
-		}finally {
-			
+			pDTO = dao.listAll(session, map, curPage);
+		} finally {
+			session.close();
 		}
+		return pDTO;
+	}// end select
+	// 게시글 목록
+
+	// 게시글 추가
+	public void write(BoardDTO dto) {
 		// TODO Auto-generated method stub
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+			dao.write(session, dto);
+			session.commit();
+		} finally {
+			session.close();
+		}
+
+	}
+	// 게시글 추가
+
+	// 게시글 수정
+	public void updateByNum(BoardDTO dto) {
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+			dao.updateByNum(session, dto);
+			session.commit();
+		} finally {
+			session.close();
+		}
+	}
+	// 게시글 수정
+
+	// 게시글 삭제
+	public void deleteNum(String num) {
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+			dao.deleteNum(session, num);
+			session.commit();
+		} finally {
+			session.close();
+		}
+
+	}
+	// 게시글 삭제
+
+	// 조회수 증가
+	public BoardDTO selectByNum(String num) {
+		SqlSession session = MySqlSessionFactory.getSession();
+		BoardDTO dto = null;
+		try {
+			dto = dao.selectByNum(session, num);
+			session.commit();
+		} finally {
+
+		}
 		return dto;
 	}
+	// 조회수 증가
+
+	//댓글
+	public void answerinsert(BoardDTO dto) {
+		SqlSession session = MySqlSessionFactory.getSession();
+		try {
+			dao.answerinsert(session, dto);
+			session.commit();
+		} finally {
+			session.close();
+		}
+
+	}
+	// 댓글
 
 	/*
 	 * public List<BoardDTO> listAll(HashMap<String, String> map) { SqlSession
@@ -37,59 +107,5 @@ public class BoardService {
 	 * 
 	 * }finally { session.close(); } return list; }
 	 */
-	public void write(BoardDTO dto) {
-		// TODO Auto-generated method stub
-		SqlSession session= MySqlSessionFactory.getSession();
-		try {
-			dao.write(session, dto);
-			session.commit();
-		}finally {
-			session.close();
-		}
-		
-	}
-	public void deleteNum(String num) {
-		SqlSession session= MySqlSessionFactory.getSession();
-		try {
-			dao.deleteNum(session, num);
-			session.commit();
-		}finally {
-			session.close();
-		}
-		
-	}
-	public void updateByNum(BoardDTO dto) {
-		SqlSession session= MySqlSessionFactory.getSession();
-		try {
-			dao.updateByNum(session, dto);
-			session.commit();
-		}finally {
-			session.close();
-		}
-		
-	}
-	public void answerinsert(BoardDTO dto) {
-		SqlSession session= MySqlSessionFactory.getSession();
-		try {
-			dao.answerinsert(session, dto);
-			session.commit();
-		}finally {
-			session.close();
-		}
-		
-	}
-	public BoardPageDTO listAll(int curPage,HashMap<String, String> map) {
-		BoardDAO dao = new BoardDAO();
-		
-		SqlSession session = MySqlSessionFactory.getSession();
-		BoardPageDTO pDTO = null;
-		 try {
-			 pDTO = dao.listAll(session,map,curPage);
-	      }finally {
-			session.close();
-	      }
-	      return pDTO;
-		}//end select
 
-	
 }
